dac3f5f368a1a7b231c19b07aa5bfc9749a93f59
Phil Nash <philnash@gmail.com>
2020-05-24 15:00:57 +1000

    Don't use babel on CJS or ESM output.

--- ad-hider/rollup.config.js
+++ ad-hider/rollup.config.js
@@ -2,20 +2,27 @@
 import pkg from "./package.json";
 import babel from "rollup-plugin-babel";
 
-export default {
-  input: "src/index.js",
-  output: [
-    {
-      name: "WebMonetizationAdHider",
-      file: pkg.browser,
-      format: "umd",
-    },
-    { file: pkg.main, format: "cjs" },
-    { file: pkg.module, format: "es" },
-  ],
-  plugins: [
-    babel({
-      exclude: "node_modules/**", // only transpile our source code
-    }),
-  ],
-};
+export default [
+  {
+    input: "src/index.js",
+    output: [
+      {
+        name: "WebMonetizationAdHider",
+        file: pkg.browser,
+        format: "umd",
+      },
+    ],
+    plugins: [
+      babel({
+        exclude: "node_modules/**", // only transpile our source code
+      }),
+    ],
+  },
+  {
+    input: "src/index.js",
+    output: [
+      { file: pkg.module, format: "es" },
+      { file: pkg.main, format: "cjs" },
+    ],
+  },
+];
